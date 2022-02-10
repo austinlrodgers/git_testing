f3929205c5c733ea1059a6bdac5107cb39d38e55
Austin Rodgers <Austin.Rodgers@starrcompanies.com>
2022-02-10 18:41:57 -0500

    the

--- new 4.bat
+++ new 4.bat
@@ -1,23 +1,21 @@
 @echo off
-set "YES=y"
 goto loop
 
 :loop
+set "YES=y"
 set "COMMIT_MESSAGE="
 set "CONTINUE="
 set /p COMMIT_MESSAGE="Please enter commit message: "
 set /p CONTINUE="Your message is: %COMMIT_MESSAGE%. Do you want to continue [y/n]?"
-echo %YES%
-echo %CONTINUE%
-if (%CONTINUE% == %YES%) (goto pushToGit %COMMIT_MESSAGE%) else (goto loop)
+if /i %CONTINUE% == %YES% call :pushToGit %COMMIT_MESSAGE%
+if /i NOT %CONTINUE% == %YES% goto loop
+echo broken
+pause
 EXIT /B 1
 	
 :pushToGit
 @echo on
-echo Entry being committed with message: %~1
-PAUSE
+echo Entry being committed with message: "%~1"
 call git add .
-call git commit %~1
+call git commit -m "%~1"
 EXIT /B 0
-
-PAUSE
