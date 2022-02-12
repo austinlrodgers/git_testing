ec9f3ee6e352cde67d51d8721971c898d6581430
Austin Rodgers <Austin.Rodgers@starrcompanies.com>
2022-02-12 12:17:18 -0500

    better

--- new 4.bat
+++ new 4.bat
@@ -10,14 +10,14 @@ set "CONTINUE="
 set /p COMMIT_MESSAGE="Please enter commit message: "
 set /p CONTINUE="Your message is: %COMMIT_MESSAGE%. Do you want to continue [y/n]?"
 if /i %CONTINUE% == %YES% (call :pushToGit %COMMIT_MESSAGE%) else (goto loop)
-goto end
+EXIT /B 0
 
 :pushToGit
 echo Entry being committed with message: "%~1"
 call git add .
 call git commit -m "%~1"
 call git push origin main
-goto end
+EXIT /B 0
 
 :end
 echo "Changes Successfully Committed"
