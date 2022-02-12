d25432188851b0fd64ee00c16fce7247c5945d71
Austin Rodgers <Austin.Rodgers@starrcompanies.com>
2022-02-12 12:16:18 -0500

    clean

--- new 4.bat
+++ new 4.bat
@@ -2,7 +2,6 @@
 call git diff --exit-code --quiet
 if %errorlevel%==0 (echo "You have no current changes";) else (goto loop)
 goto end
-EXIT /B 0
 
 :loop
 set "YES=y"
@@ -11,7 +10,7 @@ set "CONTINUE="
 set /p COMMIT_MESSAGE="Please enter commit message: "
 set /p CONTINUE="Your message is: %COMMIT_MESSAGE%. Do you want to continue [y/n]?"
 if /i %CONTINUE% == %YES% (call :pushToGit %COMMIT_MESSAGE%) else (goto loop)
-EXIT /B 0
+goto end
 
 :pushToGit
 echo Entry being committed with message: "%~1"
