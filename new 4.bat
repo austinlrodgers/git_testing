632d7ea73cf91f2dc36e14b9d28af875024620ef
Austin Rodgers <Austin.Rodgers@starrcompanies.com>
2022-02-12 12:33:45 -0500

    'who the eff knows'

--- new 4.bat
+++ new 4.bat
@@ -9,17 +9,18 @@ set "YES=y"
 set "COMMIT_MESSAGE="
 set "CONTINUE="
 set /p COMMIT_MESSAGE="Please enter commit message: "
-set /p CONTINUE="Your message is: "'%COMMIT_MESSAGE%'". Do you want to continue [y/n]?"
-if /i %CONTINUE% == %YES% (call :pushToGit %COMMIT_MESSAGE%) else (goto loop)
+set /p CONTINUE="Your message is: %COMMIT_MESSAGE%. Do you want to continue [y/n]?"
+if /i %CONTINUE% == %YES% (call :pushToGit "%COMMIT_MESSAGE%") else (goto loop)
 goto endLoop
 
 :pushToGit
 echo Entry being committed with message: %~1
 call git add .
-call git commit -m %~1
+pause
+call git commit -m "'%~1'"
+pause
 call git push origin main
-if %errorlevel%==0 (echo "Changes Successfully Committed") else (EXIT)
-
+if %errorlevel%==0 (echo "Changes Successfully Committed") else (echo "Error: %errorlevel%)
 goto endLoop
 
 :end
